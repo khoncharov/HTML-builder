207bf0fdc99643ecea6ac41755a11b62d7d5efbb
Konstantin Honcharov <k.honcharov@gmail.com>
2021-11-09 00:15:11 +0300

    fix: add error check

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -8,3 +8,7 @@ const rs = fs.createReadStream(`${path.join(__dirname, fileToRead)}`, "utf-8");
 rs.on("data", (chunk) => {
   console.log(chunk);
 });
+
+rs.on("error", (err) => {
+  console.error(err.message);
+});
